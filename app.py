b8d85b169a16ada89d7fc5034a1329d11914712b
NicsLvl <94282946+NicsLvl@users.noreply.github.com>
2023-03-31 11:02:58 +0800

    test

--- app.py
+++ app.py
@@ -174,6 +174,9 @@ st.caption("_based on a 71% accuracy rate_")
 import streamlit.components.v1 as components
 def main():
     html_temp = """<div class='tableauPlaceholder' id='viz1680230843618' style='position: relative; overflow:auto;'><noscript><a href='#'><img alt='Story 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Mc&#47;McDonaldsinSingapore_16802273308130&#47;Story1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='McDonaldsinSingapore_16802273308130&#47;Story1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Mc&#47;McDonaldsinSingapore_16802273308130&#47;Story1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1680230843618');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1016px';vizElement.style.height='991px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
-    components.html(html_temp, height=1000,width=1200)
+    st.write(f"""
+                <div style='height: 400px; overflow: auto;'>
+                {components.html(html_temp, height=1000,width=1200)}
+                </div>""",unsafe_allow_html=True)
 if __name__ == "__main__":    
     main()
